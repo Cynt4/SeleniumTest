223e0c90335aa23bd6dc7cefadb299c629eda05a
Jacek <jacek.smoliniec@gmail.com>
2022-12-06 21:18:19 +0100

    Fourth commit

--- src/test/java/WebDriverManagerTest.java
+++ src/test/java/WebDriverManagerTest.java
@@ -2,22 +2,30 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class WebDriverManagerTest {
     @Test
     public void openBrowser() {
-//        WebDriverManager.chromedriver().setup();
-//        WebDriverManager.firefoxdriver().setup();
+        WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
         WebDriverManager.edgedriver().setup();
         WebDriver chromeDriver = new ChromeDriver();
         WebDriver firefoxDriver = new FirefoxDriver();
         WebDriver edgeDriver = new EdgeDriver();
         chromeDriver.manage().window().maximize();
         Dimension windowsSize = new Dimension(200, 200);
-        edgeDriver.manage().window().setSize(windowsSize);
+        chromeDriver.manage().window().setSize(windowsSize);
+        edgeDriver.get("https://www.google.com");
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(false);
+        WebDriver driver = new ChromeDriver(options);
+
+
         chromeDriver.quit();
         edgeDriver.close();
     }
