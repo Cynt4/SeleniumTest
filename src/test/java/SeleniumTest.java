70ce29b74946de908d419e121ad9d0006a93e576
Jacek <jacek.smoliniec@gmail.com>
2022-12-07 15:00:17 +0100

    Fifth commit

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,24 +1,56 @@
-import org.openqa.selenium.InvalidArgumentException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.awt.image.Kernel;
 
 public class SeleniumTest {
 
     @Test
     public void openGooglePage() {
-        getDriver("Edge");
+        WebDriver driver = getDriver("Chrome");
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+        // Przechodzimy do okienka z plikami cookie
+//        driver.switchTo().frame(0);
+        // Znalezienie przycisku
+        WebElement agreeButton = driver.findElement(By.xpath("//div[text()= 'Zaakceptuj wszystko']"));
+        //Kliknięcie w przycisk (akceptacja plików cookie)
+        agreeButton.click();
+        //Powrót do pierwotnego okna
+        driver.switchTo().defaultContent();
+        // Znajdz pole wyszukiwania
+        WebElement searchField = driver.findElement(By.name("q"));
+        //Wprowadz w pole wyszukiwania fraze Selenium
+        searchField.sendKeys("Selenium");
+        //Zasymuluj kliknięcie przycisku enter
+        searchField.sendKeys(Keys.ENTER);
+        //Znalezienie rezultatu
+        WebElement result = driver.findElement(By.xpath("//a[contains(@href, 'selenium.dev')]//h3"));
+
+        Assert.assertTrue(result.isDisplayed());
     }
 
     public WebDriver getDriver(String browser) {
-        return switch (browser) {
-            case "Chrome" -> new ChromeDriver();
-            case "Firefox" -> new FirefoxDriver();
-            case "Edge" -> new EdgeDriver();
-            default -> throw new InvalidArgumentException("Invalid name");
-        };
+        switch (browser) {
+            case "Chrome":
+                String chromePath = "C:\\Users\\Jacek\\Downloads\\chromedriver.exe";
+                System.setProperty("webdriver.chrome.driver", chromePath);
+                return new ChromeDriver();
+            case "Firefox":
+                String firefoxPath = "\"C:\\Users\\Jacek\\Downloads\\geckodriver.exe";
+                System.setProperty("webdriver.gecko.driver", firefoxPath);
+                return new FirefoxDriver();
+            case "Edge":
+                String edgePath = "C:\\Users\\Jacek\\Downloads\\edgedriver_win64\\msedgedriver.exe";
+                System.setProperty("webdriver.edge.driver", edgePath);
+                return new EdgeDriver();
+            default:
+                throw new InvalidArgumentException("Invalid name");
+        }
     }
 
 }
